4d7e97aa5a8c19b460e5c07059d3add0fa256fc8
Lugard <lanlugar@gmail.com>
2019-05-26 08:40:56 +0100

    Signed-off-by: Lugard <lanlugar@gmail.com>

--- src/js/base.js
+++ src/js/base.js
@@ -1,3 +1,5 @@
 export default {
-    map: 'map'
+    map: 'map',
+    senateBtn: document.querySelector('.senate__btn'),
+    repsBtn: document.querySelector('.reps__btn')
 }
--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,7 @@ const mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
 const repsData = require('../data/reps.json');
 const senateData = require('../data/senate.json');
 
+
 import $ from 'jquery';
 import {accesstoken} from '../../configurations';
 import mapConfig from './map.config';
@@ -10,4 +11,9 @@ import elements from './base';
 mapboxgl.accessToken =  accesstoken;
 const map = new mapboxgl.Map(mapConfig);
 
-console.log(repsData);
+
+elements.repsBtn.addEventListener('click', e => {
+    //1.add senate layer 
+    //2. remove active class from all btn and add to this btn
+    //3. close all filter for the other categories and open for party
+})
